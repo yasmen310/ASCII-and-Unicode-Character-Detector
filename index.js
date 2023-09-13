083e408f1908a2ca6821f1516b46aba1c6276abc
Yasmen Ahmed <130899829+yasmen310@users.noreply.github.com>
2023-09-13 06:47:28 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function detectCharacter(){
+  const characterInput=document.getElementById("characterInput");
+  const result=document.getElementById("result");
+
+  const character=characterInput.value;
+  if(isASCII(character)){
+    result.innerText="The entered character is an ASCII Character";
+  }
+  else{
+    result.innerText="The entered character is an Unicode Character";
+
+  }
+}
+function isASCII(character){
+  return character.charCodeAt(0)<=127;
+}
